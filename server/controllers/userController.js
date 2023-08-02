cc2df37359aba2828d40d872af933fb09abfe685
doronlevy6 <doronlevy66@gmail.com>
2023-08-02 15:05:46 +0300

    sending back loging user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,8 +5,6 @@ const userService = require("../services/userService");
 router.post("/register", async (req, res) => {
   const { username, password, email } = req.body;
   try {
-    console.log("\n  req.body1", req.body, "\n");
-
     const user = await userService.createUser(username, password, email);
     res.status(201).json({ success: true, user: user });
   } catch (err) {
@@ -19,10 +17,23 @@ router.post("/login", async (req, res) => {
   try {
     const user = await userService.loginUser(username, password);
     if (user) {
-      res.status(200).json({ success: true });
+      res.status(200).json({ success: true, user });
     } else {
       res.status(401).json({ success: false, message: "Invalid credentials" });
     }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+router.get("/usernames", async (req, res) => {
+  try {
+    const usernames = await userService.getAllUsernames();
+    console.log("\n usernames", usernames, "\n");
+
+    res
+      .status(200)
+      .json({ success: true, usernames: usernames.map((u) => u.username) });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
